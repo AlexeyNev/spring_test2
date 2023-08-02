845b2bfdcd517bf289e04b06c7d877fed120489b
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-08-02 20:03:49 +0400

    @Autowired

--- src/main/java/ru/nevredimov/springcourse/TestSpring.java
+++ src/main/java/ru/nevredimov/springcourse/TestSpring.java
@@ -7,17 +7,8 @@ public class TestSpring {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        musicPlayer.playMusic();
-
-        Music music1 = context.getBean("rapMusic", Music.class);
-        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
-        musicPlayer1.playMusic();
-
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-        musicPlayer2.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
