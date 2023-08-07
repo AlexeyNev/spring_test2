e6397d1bd0d188fdf0ce59830dbed9acb1687171
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-08-07 20:07:16 +0400

    @Scope @Value @PostConstruct @PreDestroy

--- src/main/java/ru/nevredimov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/nevredimov/springcourse/ClassicalMusic.java
@@ -1,23 +1,27 @@
 package ru.nevredimov.springcourse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class ClassicalMusic implements Music {
-    private List<String> songs = new ArrayList<>();
-
-    {
-        songs.add("Hungarian Rhapsody");
-        songs.add("Symphony no. 5 in C Minor, op. 67");
-        songs.add("Night on Bald Mountain");
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("doMyInit");
     }
 
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("doMyDestroy");
+    }
 
     @Override
-    public List<String> getSongs() {
-        return songs;
+    public String getSong() {
+        return "Bogemian Phapsody";
     }
 }
--- src/main/java/ru/nevredimov/springcourse/Music.java
+++ src/main/java/ru/nevredimov/springcourse/Music.java
@@ -3,5 +3,5 @@ package ru.nevredimov.springcourse;
 import java.util.List;
 
 public interface Music {
-    List<String> getSongs();
+    public String getSong();
 }
